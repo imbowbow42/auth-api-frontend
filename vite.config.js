44b351548dc0a7127bff1c79f49258f42191d007
imbowbow42 <phamgbao42@gmail.com>
2026-03-21 14:12:08 +0700

    fix: explicit MPA configuration to ensure home.html is built

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   server: {
     port: 3300,
     strictPort: true, // Fail if port 3000 is taken, to ensure we match backend CORS
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        home: resolve(__dirname, 'home.html'),
+      },
+    },
   },
 });
